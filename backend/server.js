9150cabdded2f57a3a0d653f2727230159691a34
abdulaziz <aoqilov88@gmail.com>
2025-01-23 20:35:52 +0500

    remove node-modules

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import productsRouter from "./routes/product.route.js";
 
@@ -8,9 +9,20 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 
 app.use("/api/products", productsRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 // ASOSIY PROYEKT
 app.listen(PORT, () => {
   connectDB();
